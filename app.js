8d2e5b907cacc7e57724f5842282fc0916e4804e
lidunlife <uchihalidun@gmail.com>
2024-04-12 21:14:38 +0500

    2nd-lesson-4

---
+++ app.js
@@ -0,0 +1,7 @@
+let str = prompt("Write the string")
+let num = +prompt(`Write the number`)
+let bool = prompt("Write the boolean")
+
+alert(typeof str)
+alert( typeof num)
+alert( bool = `boolean`)
